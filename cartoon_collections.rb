fbf5c259ad0cfb1a0fb64b7ca9275f1d88161457
Oliver Feher <olcsee@gmail.com>
2020-01-24 07:05:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,7 @@ def long_planeteer_calls(array)
   array.any? {|element| element.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|element| element.include?("#{cheese_types}")}
 end
